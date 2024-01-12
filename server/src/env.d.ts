aa0981cd2706a2cf04331e23e1956b3a3ebc4b85
Samdeesh <samdeeshsingh001@gmail.com>
2024-01-12 14:27:37 +0530

    Add global error handler

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      BASE_API_URL: string;
       NODE_ENV: 'development' | 'production';
       PORT: string;
       DB_CONNECTION_URI: string;
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import httpLogger from 'morgan';
 import connectDB from './config/db';
 dotenv.config();
+import errorHandler from './middlewares/errorHandler';
 
 /* CONFIGURATION */
 const server = express();
@@ -19,6 +20,7 @@ const baseUrl = process.env.BASE_API_URL || '/api';
 server.get(baseUrl, (_, res) => {
   return res.json({ success: true, message: 'The server is up' });
 });
+server.use(errorHandler);
 
 connectDB();
 
