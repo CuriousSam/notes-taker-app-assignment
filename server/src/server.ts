0b2ac2b38b1802c2fdc4fceb4c808c96440013cd
Samdeesh <samdeeshsingh001@gmail.com>
2024-01-12 13:57:22 +0530

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import httpLogger from 'morgan';
+dotenv.config();
+
+/* CONFIGURATION */
+const server = express();
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors({ origin: '*' }));
+server.use(helmet());
+server.use(httpLogger('dev'));
+
+/* ROUTES */
+const baseUrl = process.env.BASE_API_URL || '/api';
+server.get(baseUrl, (_, res) => {
+  return res.json({ success: true, message: 'The server is up' });
+});
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log('Server is listening at port: ', port);
+});
