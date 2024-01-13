cc336fbe64a92ee000b8728dd1c44e3e670907b6
Samdeesh <samdeeshsingh001@gmail.com>
2024-01-13 18:39:41 +0530

    Add links in navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
+import UserIcon from '@mui/icons-material/AccountCircleOutlined';
 import BookIcon from '@mui/icons-material/BookOutlined';
+import { Tooltip } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -6,12 +8,18 @@ import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom';
-import { logout } from '../../state/features/auth';
+import { logout, selectUser } from '../../state/features/auth';
 import { useAppDispatch } from '../../state/hooks/useAppDispatch';
+import { useAppSelector } from '../../state/hooks/useAppSelector';
 
 const Navbar = () => {
+  const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate('/notes');
+  };
 
   const handleLogout = () => {
     dispatch(logout());
@@ -28,12 +36,23 @@ const Navbar = () => {
             color='inherit'
             aria-label='menu'
             sx={{ mr: 2 }}
+            onClick={navigateToHome}
           >
             <BookIcon />
           </IconButton>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          <Typography
+            onClick={navigateToHome}
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1 }}
+          >
             Notes Taker
           </Typography>
+          <Tooltip title={`Logged in as ${user!.name}`}>
+            <IconButton>
+              <UserIcon htmlColor='white' />
+            </IconButton>
+          </Tooltip>
           <Button onClick={handleLogout} color='inherit'>
             Log Out
           </Button>
